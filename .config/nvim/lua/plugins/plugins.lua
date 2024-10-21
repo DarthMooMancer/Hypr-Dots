3753ea9371b72d17cd892d40ec529e9006985258
DarthMooMancer <haynes.a467@gmail.com>
2024-10-21 19:04:34 -0400

    Cleaning up Neovim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,5 +1,8 @@
 return {
 	"nvim-telescope/telescope.nvim", dependencies = {"nvim-lua/plenary.nvim"},
+	"numToStr/Comment.nvim",
+	"norcalli/nvim-colorizer.lua",
+	"kylechui/nvim-surround",
 	{ "catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
@@ -7,15 +10,7 @@ return {
 			vim.cmd "colorscheme catppuccin-frappe"
 		end
 	},
-	"numToStr/Comment.nvim",
-	"norcalli/nvim-colorizer.lua",
-	{ "windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {
-			check_ts = true,
-		}
-	},
-	"kylechui/nvim-surround",
+	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = { check_ts = true }},
   { "hrsh7th/nvim-cmp",
 		dependencies = {
 			"cmp-nvim-lsp",
@@ -36,12 +31,53 @@ return {
       config.setup({
         ensure_installed = {"lua", "python", "rust"},
         auto_install = true,
-        highlight = {
-					enable = true,
-				  additional_vim_regex_highlighting = false
-				},
+        highlight = { enable = true, additional_vim_regex_highlighting = false },
         indent = { enable = true },
       })
     end
-  }
+  },
+	{ 'neovim/nvim-lspconfig',
+		dependencies  = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+
+		config = function ()
+			require('mason').setup()
+			require('mason-lspconfig').setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"rust_analyzer",
+				},
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup{}
+					end,
+				},
+			})
+		end
+	},
+	{	"mfussenegger/nvim-lint",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				python = { "pylint" },
+			}
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 }
